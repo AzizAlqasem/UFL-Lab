f906d41794545f062158c92fd1eafbec19dd7736
Abdulaziz Alqasem <45468407+AzizAlqasem@users.noreply.github.com>
2021-12-26 07:24:24 +0000

    latex

--- gui/nlo.py
+++ gui/nlo.py
@@ -9,6 +9,6 @@ def main():
     st.title("Nonlinear Optics Calculator")
     st.markdown("""#### Sum Freq Generator""")
     c1, c2 = st.columns(2)
-    wl1 = c1.number_input("Wavelength 1 (nm)", value=800.0, key=1)
-    wl2 = c2.number_input("Wavelength 2 (nm)", value=800.0, key=2)
+    wl1 = c1.number_input("Wavelength 1 (nm)", value=800.0, step=50.0, key=1)
+    wl2 = c2.number_input("Wavelength 2 (nm)", value=800.0, step=50.0, key=2)
     st.latex(to_latex("Wavelength out", sum_freq(wl1, wl2), unit='nm'))
