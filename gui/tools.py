d6a37b7dd2c1a5373e26176214b4702c11fe6d7d
Abdulaziz Alqasem <45468407+AzizAlqasem@users.noreply.github.com>
2021-12-26 07:15:05 +0000

    latex

--- gui/tools.py
+++ gui/tools.py
@@ -1,9 +1,11 @@
 
 
 
-def to_latex(text, x, round_digits=4, scientific_notation=False):
+def to_latex(text, x, round_digits=4, unit=None, scientific_notation=False):
     if scientific_notation:
         res =  "%.*e" % (round_digits, x)
     else:
         res = "%.*f" % (round_digits, x)
+    if unit is not None:
+        res += " " + unit
     return r"\text{" + text + "} = " + res
