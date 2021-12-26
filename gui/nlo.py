bb420839bb0b195ab86b8fa789a975ef97ab57aa
Abdulaziz Alqasem <45468407+AzizAlqasem@users.noreply.github.com>
2021-12-26 07:23:19 +0000

    latex

--- gui/nlo.py
+++ gui/nlo.py
@@ -1,14 +1,14 @@
 import streamlit as st
-
+from gui.tools import to_latex
 from src.nlo import sum_freq
-
-from src.nlo import *
+#from src.nlo import *
 
 
 
 def main():
     st.title("Nonlinear Optics Calculator")
-    st.markdown("""## Sum Freq Generator""")
-    wl1 = st.number_input("Wavelength 1 (nm)", value=800.0)
-    wl2 = st.number_input("Wavelength 2 (nm)", value=800.0)
-    st.write("Wavelength out (nm)", sum_freq(wl1, wl2))
+    st.markdown("""#### Sum Freq Generator""")
+    c1, c2 = st.columns(2)
+    wl1 = c1.number_input("Wavelength 1 (nm)", value=800.0, key=1)
+    wl2 = c2.number_input("Wavelength 2 (nm)", value=800.0, key=2)
+    st.latex(to_latex("Wavelength out", sum_freq(wl1, wl2), unit='nm'))
